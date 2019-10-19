01cd038939ce336613c87ec7574d0f20cda36ddd
@xanderlav <alejandroregino.hussmann.com>
2019-10-19 13:59:12 -0500

    1st commit of this project

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+// Define global variables
+var count = 0;
+
+// Define firebase connection & settings
+
+
+// Store new element in table
+function sendNewTrain(){
+
+}
+
+// Get elements in table
+funciont getAllTrains(){
+    
+}
